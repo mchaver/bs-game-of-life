921392dd2737cab4feeb64e2797a6694874d7b8c
James Haver <mchaver@gmail.com>
2018-06-24 16:17:17 +0800

    Add speed option

--- src/GameOfLife.ml
+++ src/GameOfLife.ml
@@ -52,7 +52,7 @@ module Canvas = struct
   external scrollTop : t -> int = "" [@@bs.get]
   external offsetParent : t -> Element.t option = "" [@@bs.get] [@@bs.return null_to_opt]
 end
-                  
+
 module CanvasEvent = struct
   type t
   external clientX : t -> int = "" [@@bs.get]
@@ -62,8 +62,25 @@ module CanvasEvent = struct
   external pageY : t -> int = "" [@@bs.get]                              
   external addEventListener : string -> (t -> unit) -> unit = "" [@@bs.send.pipe: Canvas.t]
   external addClickEventListener : (_ [@bs.as "click"]) -> (t -> unit) -> unit = "addEventListener" [@@bs.send.pipe : Canvas.t]
-end              
+end
 
+module Option = struct
+  type t
+  external value : t -> string = "" [@@bs.get]
+end
+                   
+module Select = struct
+  type t
+  external getElementById : string -> t option = "document.getElementById" [@@bs.val] [@@bs.return null_to_opt]
+  external selectedIndex : t -> int = "" [@@bs.get]
+  external options : t -> Option.t array = "" [@@bs.get]
+end               
+
+module SelectEvent = struct
+  type t
+  external addEventListener : string -> (t -> unit) -> unit = "" [@@bs.send.pipe: Select.t]
+  external addChangeEventListener : (_ [@bs.as "change"]) -> (t -> unit) -> unit = "addEventListener" [@@bs.send.pipe : Select.t]
+end              
 
 let isNone o =
   match o with
@@ -114,6 +131,31 @@ function relMouseCoords(event){
     return {x:canvasX, y:canvasY}
 }
          *)                  
+
+type speed =
+  | VerySlow
+  | Slow
+  | Normal
+  | Fast
+  | VeryFast
+
+let parse_speed v =
+  match v with
+  | "very-slow" -> Some VerySlow
+  | "slow" -> Some Slow
+  | "normal" -> Some Normal
+  | "fast" -> Some Fast
+  | "very-fast" -> Some VeryFast
+  | _ -> None
+
+let speed_to_int s =
+  match s with
+  | VerySlow -> 1000
+  | Slow -> 500
+  | Normal -> 100
+  | Fast -> 50
+  | VeryFast -> 0
+                 
 type cell =
   | Dead
   | Alive
@@ -131,6 +173,7 @@ type state =
   ; grid    : (cell array) array
   ; rows    : int
   ; columns : int
+  ; speed   : speed
   }
 
 let state: state ref =
@@ -139,6 +182,7 @@ let state: state ref =
     ; grid = [||]
     ; rows = 0
     ; columns = 0
+    ; speed = Normal
     }
                      
 let clear canvas context = (
@@ -249,6 +293,7 @@ let next grid =
       | 2 -> cell
       | _ -> Dead
 
+(* throw away result and return unit *)
 let void _x = ()
            
 let rec run canvas =
@@ -263,7 +308,7 @@ let rec run canvas =
       Window.requestAnimationFrame @@ fun () -> run canvas
     end
   else ()
-    ) 100
+    ) @@ speed_to_int !state.speed
 
 let resetGrid rows columns =
   Random.self_init ();
@@ -292,6 +337,11 @@ let main =
     | None -> failwith "Cannot find the random-reset-button"
     | Some random_reset_button -> random_reset_button in
 
+  let speed_select =
+    match (Select.getElementById "speed-select") with
+    | None -> failwith "Cannot find the speed-select"
+    | Some speed_select -> speed_select in
+  
   let handleClickGrid (e: CanvasEvent.t) = (
     Js.log("handleClickGrid");
     (* let x = CanvasEvent.clientX e in *)
@@ -327,6 +377,15 @@ let main =
     else Mouse.setInnerHTML play_button "Play" in
 
   play_button |> MouseEvent.addClickEventListener (fun _unit -> toggleRun ());
+
+  let handleSpeedSelectChange _event =(
+    let selectedIndex = Select.selectedIndex speed_select in
+    let option = (Select.options speed_select).(selectedIndex) in
+    match parse_speed @@ Option.value option with
+    | None -> () 
+    | Some speed -> state := {!state with speed = speed}) in
+
+  speed_select |> SelectEvent.addChangeEventListener handleSpeedSelectChange;
 
   let randomGrid _unit =
     resetGrid !state.rows !state.columns;
